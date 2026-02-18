d65acc5a81d6e8f4374e6d6bf178351084808374
c9sid <kumarsiddharth7666@gmail.com>
2026-02-18 18:34:19 +0530

    Admin page

--- app/admin/page.jsx
+++ app/admin/page.jsx
@@ -4,10 +4,12 @@ export default async function AdminPage() {
   const user = await currentUser();
 
   return (
-    <div className="p-10">
+    <section className="h-screen">
+      <div className="flex items-start justify-start mx-auto max-w-7xl pt-32 px-5">
       <h1 className="text-3xl font-bold">
         Welcome {user?.firstName}
       </h1>
     </div>
+    </section>
   );
 }
